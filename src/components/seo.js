36ecd4c115030f4f8cbe953581bd53b0f7132b43
Rinat Rezyapov <rinat.rezyapov@gmail.com>
2020-11-21 20:23:40 +0500

    Logo svg is added to twitter image

--- src/components/seo.js
+++ src/components/seo.js
@@ -71,7 +71,7 @@ const SEO = ({ description, lang, meta, title }) => {
         },
         {
           name: `twitter:image`,
-          content: 'https://rinat-rezyapov.netlify.app/icons/icon-512x512.png',
+          content: 'https://rinat-rezyapov.netlify.app/logo.svg',
         },
       ].concat(meta)}
     />
